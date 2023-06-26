1052a077d330d2aab32d044e131ad95eae6a39f2
Gautier DI FOLCO <gautier.difolco@gmail.com>
2023-06-26 20:11:55 +0200

    fix(tool): use nix caches

--- code/hsec-tools/flake.nix
+++ code/hsec-tools/flake.nix
@@ -1,7 +1,13 @@
 {
   description = "hsec-tools";
 
-  # useless comment
+
+  nixConfig = {
+    extra-substituters = "https://cache.nixos.org https://hydra.nixos.org https://nix-community.cachix.org";
+    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    # extra-experimental-features = "nix-command flakes";
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
