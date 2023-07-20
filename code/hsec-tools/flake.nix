7111c8c6d85a11c5cd557984109d067295424d5b
case <case@capsulecorp.org>
2023-07-20 15:20:01 -0400

    tools: swap to different flake layout/update inputs

--- code/hsec-tools/flake.nix
+++ code/hsec-tools/flake.nix
@@ -7,40 +7,35 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-
-          jailbreakUnbreak = pkg:
-            pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.unmarkBroken pkg));
-
-          haskellPackages = pkgs.haskell.packages.ghc925.override
-            {
-              overrides = hself: hsuper: {
-                Cabal-syntax = hsuper.Cabal-syntax_3_8_1_0;
-              };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs =
+          import nixpkgs { inherit system overlays; config.allowBroken = true; };
+        project = returnShellEnv:
+          pkgs.haskellPackages.developPackage {
+            inherit returnShellEnv;
+            name = "hsec-tools";
+            root = ./.;
+            withHoogle = false;
+            overrides = self: super: with pkgs.haskell.lib; {
             };
-        in
-        rec
-        {
-          packages.hsec-tools =
-            haskellPackages.callCabal2nix "hsec-tools" ./. {
-              # Dependency overrides go here
-            };
-
-          defaultPackage = packages.hsec-tools;
-
-          devShell =
-            pkgs.mkShell {
-              buildInputs = with haskellPackages; [
-                haskell-language-server
-                ghcid
+            modifier = drv:
+              pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+              [
+                cabal-fmt
                 cabal-install
-              ];
-              inputsFrom = [
-                self.defaultPackage.${system}.env
-              ];
-            };
+                ghcid
+                haskell-language-server
+                pkgs.nixpkgs-fmt
+              ]);
+          };
+      in
+      {
+        # Used by `nix build` & `nix run` (prod exe)
+        defaultPackage = project true;
+
+        # Used by `nix develop` (dev shell)
+        devShell = project true;
         });
 }
