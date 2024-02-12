b6d99a722f7221f9d56151d6d1b3c78747972188
mangoiv <mail@mangoiv.com>
2024-02-13 00:47:47 +0100

    [feat] add part of the implementation of looking up advisories in the DB

--- code/hsec-cabal/src/Distribution/Audit.hs
+++ code/hsec-cabal/src/Distribution/Audit.hs
@@ -1,7 +1,6 @@
 module Distribution.Audit (auditMain) where
 
 import Data.Foldable (traverse_)
-import qualified Distribution.Client.InstallPlan as Plan
 import Distribution.Client.NixStyleOptions
   ( NixStyleFlags (configFlags)
   , defaultNixStyleFlags
@@ -26,14 +25,39 @@ import Distribution.Simple.Command
   ( CommandParse (CommandErrors, CommandHelp, CommandList, CommandReadyToGo)
   , CommandUI (..)
   , commandParseArgs
+  , mkCommandUI
   )
 import Distribution.Simple.Flag (fromFlagOrDefault)
 import qualified Distribution.Verbosity as Verbosity
 import System.Environment (getArgs)
+import qualified System.FilePath as FP
+import Control.Exception (throwIO, Exception (displayException))
+import Security.Advisories.Filesystem (listAdvisories)
+import Validation (validation)
+import Security.Advisories (ParseAdvisoryError)
+import GHC.Generics (Generic)
+import Security.Advisories.Cabal (matchAdvisoriesForPlan)
+
+data AuditException 
+  = MissingArgs  
+  | TooManyArgs
+  | InvalidFilePath String
+  | ListAdvisoryValidationError FilePath [ParseAdvisoryError]
+  deriving stock (Eq, Show, Generic)
+
+instance Exception AuditException where
+ displayException = \case 
+   MissingArgs -> "You didn't specify where to take the audit results from"
+   TooManyArgs -> "Expected only one argument"
+   InvalidFilePath fp -> show fp <> " is not a valid filepath"
+   ListAdvisoryValidationError dir errs -> unlines 
+     [ "Listing the advisories in directory " <> dir <> " failed with:"
+     , show errs
+     ]
 
 auditMain :: IO ()
 auditMain =
-  handleArgs auditCommandUI \flags -> do
+  handleArgs auditCommandUI \args flags -> do
     let verbosity = verbosityFromFlags flags
         cliConfig = projectConfigFromFlags flags
     ProjectBaseContext {distDirLayout, cabalDirLayout, projectConfig, localPackages} <-
@@ -41,9 +65,19 @@ auditMain =
         verbosity
         cliConfig
         OtherCommand
-    (_, plan, _, _, _) <-
+    (plan', plan, _, _, _) <-
       rebuildInstallPlan verbosity distDirLayout cabalDirLayout projectConfig localPackages Nothing
-    print `traverse_` Plan.toList plan
+
+    fp <- case args of 
+      [] -> throwIO MissingArgs
+      [fp] -> if FP.isValid fp then pure fp else throwIO (InvalidFilePath fp) 
+      (_x : _y : _zs) -> throwIO TooManyArgs
+    advisories <- listAdvisories fp 
+      >>= validation (throwIO . ListAdvisoryValidationError fp) pure
+
+    print $ matchAdvisoriesForPlan plan advisories
+    print $ matchAdvisoriesForPlan plan' advisories
+    -- TODO(mangoiv): find out what's the correct plan
 
 projectConfigFromFlags :: NixStyleFlags a -> ProjectConfig
 projectConfigFromFlags flags = commandLineFlagsToProjectConfig defaultGlobalFlags flags mempty
@@ -53,25 +87,22 @@ verbosityFromFlags = fromFlagOrDefault Verbosity.normal . configVerbosity . conf
 
 auditCommandUI :: CommandUI (NixStyleFlags ())
 auditCommandUI =
-  CommandUI
-    { commandName = "cabal-audit"
-    , commandSynopsis = "Audits your cabal project"
-    , commandUsage = ("Usage: " ++)
-    , commandDescription = Nothing
-    , commandNotes = Nothing
-    , commandDefaultFlags = defaultNixStyleFlags ()
-    , commandOptions = nixStyleOptions (const [])
-    }
+  mkCommandUI
+    "audit"
+    "Audits your cabal project"
+    ["<hsec-advisory-directory>"]
+    do defaultNixStyleFlags ()
+    do nixStyleOptions (const [])
 
 -- | handle cabal global command args
 handleArgs
   :: CommandUI flags
-  -> (flags -> IO ())
+  -> ([String] -> flags -> IO ())
   -> IO ()
 handleArgs ui k = do
   args <- getArgs
-  case commandParseArgs ui True args of
-    CommandHelp help -> putStrLn $ help "cabal-audit"
-    CommandList opts -> putStrLn $ "commandList: " <> show opts
-    CommandErrors errs -> putStrLn $ "commandErrors: " <> show errs
-    CommandReadyToGo (flags, _commandParse) -> k $ flags $ commandDefaultFlags ui
+  case commandParseArgs ui False args of
+    CommandHelp help -> putStrLn $ help "cabal"
+    CommandList opts -> putStrLn `traverse_` opts
+    CommandErrors errs -> putStrLn `traverse_` errs
+    CommandReadyToGo (flags, commandParse) -> k commandParse $ flags $ commandDefaultFlags ui
--- code/hsec-cabal/src/Security/Advisories/Cabal.hs
+++ code/hsec-cabal/src/Security/Advisories/Cabal.hs
@@ -1,4 +1,90 @@
-module Security.Advisories.Cabal where
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE UndecidableInstances #-}
 
+module Security.Advisories.Cabal (matchAdvisoriesForPlan) where
 
+import Data.Functor.Identity (Identity (Identity))
+import Data.Kind (Type)
+import Data.Map (Map, (!?))
+import Data.Map.Strict qualified as Map
+import Data.Maybe (mapMaybe)
+import Data.Monoid (Any (Any, getAny))
+import Data.Proxy (Proxy (Proxy))
+import Data.Text qualified as T
+import Distribution.Client.InstallPlan (foldPlanPackage)
+import Distribution.Client.InstallPlan qualified as Plan
+import Distribution.Client.ProjectPlanning (ElaboratedInstallPlan, elabPkgSourceId)
+import Distribution.InstalledPackageInfo (sourcePackageId)
+import Distribution.Package (PackageIdentifier (PackageIdentifier, pkgName, pkgVersion), PackageName, mkPackageName)
+import Distribution.Version (Version)
+import GHC.Generics (Generic)
+import Security.Advisories (Advisory (advisoryAffected), Affected (Affected, affectedPackage, affectedVersions), AffectedVersionRange (affectedVersionRangeIntroduced))
 
+-- | for a given 'ElaboratedInstallPlan' and a list of advisories, construct a map of advisories
+--   and packages within the install plan that are affected by them
+matchAdvisoriesForPlan
+  :: ElaboratedInstallPlan
+  -- ^ the plan as created by cabal
+  -> [Advisory]
+  -- ^ the advisories as discovered in some advisory dir
+  -> Map PackageName ElaboratedPackageInfoAdvised
+matchAdvisoriesForPlan plan = foldr advise Map.empty
+ where
+  advise :: Advisory -> Map PackageName ElaboratedPackageInfoAdvised -> Map PackageName ElaboratedPackageInfoAdvised
+  advise adv = do
+    let versionAffected :: Version -> [AffectedVersionRange] -> Bool
+        versionAffected v = getAny . foldMap (Any . (== v) . affectedVersionRangeIntroduced)
+
+        advPkgs :: [(PackageName, ElaboratedPackageInfoAdvised)]
+        advPkgs = flip mapMaybe (advisoryAffected adv) \Affected {affectedPackage, affectedVersions} -> do
+          let pkgn = mkPackageName (T.unpack affectedPackage)
+          MkElaboratedPackageInfoWith {elaboratedPackageVersion = elabv} <- installPlanToLookupTable plan !? pkgn
+          if versionAffected elabv affectedVersions
+            then Just (pkgn, MkElaboratedPackageInfoWith {elaboratedPackageVersion = elabv, packageAdvisories = Identity [adv]})
+            else Nothing
+
+    flip
+      do foldr . uncurry $ Map.insertWith combinedElaboratedPackageInfos
+      advPkgs
+
+  combinedElaboratedPackageInfos
+    MkElaboratedPackageInfoWith {elaboratedPackageVersion = ver1, packageAdvisories = advs1}
+    MkElaboratedPackageInfoWith {packageAdvisories = advs2} =
+      MkElaboratedPackageInfoWith {elaboratedPackageVersion = ver1, packageAdvisories = advs1 <> advs2}
+
+type ElaboratedPackageInfoAdvised = ElaboratedPackageInfoWith Identity
+
+type ElaboratedPackageInfo = ElaboratedPackageInfoWith Proxy
+
+-- | information about the elaborated package that
+--   is to be looked up that we want to add  to the
+--   information displayed in the advisory
+type ElaboratedPackageInfoWith :: (Type -> Type) -> Type
+data ElaboratedPackageInfoWith f = MkElaboratedPackageInfoWith
+  { elaboratedPackageVersion :: Version
+  -- ^ the version of the package that is installed
+  , packageAdvisories :: f [Advisory]
+  }
+  deriving stock (Generic)
+
+deriving stock instance Eq (f [Advisory]) => (Eq (ElaboratedPackageInfoWith f))
+
+deriving stock instance Ord (f [Advisory]) => (Ord (ElaboratedPackageInfoWith f))
+
+deriving stock instance Show (f [Advisory]) => (Show (ElaboratedPackageInfoWith f))
+
+--   FUTUREWORK(mangoiv): this could probably be done more intelligent by also
+--   looking up via the version range but I don't know exacty how
+
+-- | 'Map' to lookup the package name in the install plan that returns information
+--   about the package
+installPlanToLookupTable :: ElaboratedInstallPlan -> Map PackageName ElaboratedPackageInfo
+installPlanToLookupTable = Map.fromList . fmap planPkgToPackageInfo . Plan.toList
+ where
+  planPkgToPackageInfo pkg = do
+    let (PackageIdentifier {pkgName, pkgVersion}) =
+          foldPlanPackage
+            sourcePackageId
+            elabPkgSourceId
+            pkg
+    (pkgName, MkElaboratedPackageInfoWith {elaboratedPackageVersion = pkgVersion, packageAdvisories = Proxy})
