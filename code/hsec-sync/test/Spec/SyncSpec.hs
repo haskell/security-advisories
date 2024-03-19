5d3aea916a0f8c5660e87b479f246dd81c5586cf
Gautier DI FOLCO <gautier.difolco@gmail.com>
2024-03-19 23:20:47 +0100

    feature: add hsec-sync status

--- code/hsec-sync/test/Spec/SyncSpec.hs
+++ code/hsec-sync/test/Spec/SyncSpec.hs
@@ -20,36 +20,49 @@ spec =
     [ testGroup
         "sync"
         [ testCase "Invalid root should fail" $ do
-            result <- sync $ withRepositoryAt "/x/y/z"
-            first (const ("<Redacted error>" :: String)) result @?= Left "<Redacted error>",
+            let repo = withRepositoryAt "/dev/advisories"
+            status repo >>= (@?= DirectoryMissing)
+            result <- sync repo
+            first (const ("<Redacted error>" :: String)) result @?= Left "<Redacted error>"
+            status repo >>= (@?= DirectoryMissing),
           testCase "Subdirectory creation should work" $
             withSystemTempDirectory "hsec-sync" $ \p -> do
-              result <- sync $ withRepositoryAt $ p </> "repo"
-              result @?= Right Created,
+              let repo = withRepositoryAt $ p </> "repo"
+              status repo >>= (@?= DirectoryMissing)
+              result <- sync repo
+              result @?= Right Created
+              status repo >>= (@?= DirectoryUpToDate),
           testCase "With existing subdirectory creation should work" $
             withSystemTempDirectory "hsec-sync" $ \p -> do
               D.createDirectory $ p </> "repo"
-              result <- sync $ withRepositoryAt $ p </> "repo"
+              let repo = withRepositoryAt $ p </> "repo"
+              result <- sync repo
               result @?= Right Created,
           testCase "Sync twice should be a no-op" $
             withSystemTempDirectory "hsec-sync" $ \p -> do
-              resultCreate <- sync $ withRepositoryAt p
+              let repo = withRepositoryAt p
+              status repo >>= (@?= DirectoryEmpty)
+              resultCreate <- sync repo
               resultCreate @?= Right Created
-              resultResync <- sync $ withRepositoryAt p
+              resultResync <- sync repo
               resultResync @?= Right AlreadyUpToDate,
           testCase "Sync behind should update" $
             withSystemTempDirectory "hsec-sync" $ \p -> do
-              resultCreate <- sync $ withRepositoryAt p
+              let repo = withRepositoryAt p
+              resultCreate <- sync repo
               resultCreate @?= Right Created
               D.withCurrentDirectory p $ do
-                (status, _, _) <-
+                (statusReset, _, _) <-
                   readProcessWithExitCode "git" ["reset", "--hard", "HEAD~50"] ""
-                status @?= ExitSuccess
-              resultResync <- sync $ withRepositoryAt p
-              resultResync @?= Right Updated,
+                statusReset @?= ExitSuccess
+              status repo >>= (@?= DirectoryOutDated)
+              resultResync <- sync repo
+              resultResync @?= Right Updated
+              status repo >>= (@?= DirectoryUpToDate),
           testCase "Sync behind and changed remote should update" $
             withSystemTempDirectory "hsec-sync" $ \p -> do
-              resultCreate <- sync $ withRepositoryAt p
+              let repo = withRepositoryAt p
+              resultCreate <- sync repo
               resultCreate @?= Right Created
               D.withCurrentDirectory p $ do
                 (statusReset, _, _) <-
@@ -58,7 +71,7 @@ spec =
                 (statusRemote, _, _) <-
                   readProcessWithExitCode "git" ["remote", "rename", "origin", "old"] ""
                 statusRemote @?= ExitSuccess
-              resultResync <- sync $ withRepositoryAt p
+              resultResync <- sync repo
               resultResync @?= Right Updated
         ]
     ]
