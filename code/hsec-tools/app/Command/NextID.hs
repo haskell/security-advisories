9aa39f51b79e831bc793d33457983c75f1c88148
Fraser Tweedale <frase@frase.id.au>
2024-08-02 13:26:32 +1000

    tools: refactor: extract repo checks to utility method

--- code/hsec-tools/app/Command/NextID.hs
+++ code/hsec-tools/app/Command/NextID.hs
@@ -1,24 +1,10 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Command.NextID where
 
-import Control.Monad (unless)
-import Data.Maybe (fromMaybe)
-import System.Exit (die)
-
-import Security.Advisories.Git (getRepoRoot)
 import Security.Advisories.Core.HsecId (printHsecId, getNextHsecId)
-import Security.Advisories.Filesystem (isSecurityAdvisoriesRepo, getGreatestId)
+import Security.Advisories.Filesystem (getGreatestId)
+
+import Util (ensureRepo)
 
 runNextIDCommand :: Maybe FilePath -> IO ()
-runNextIDCommand mPath = do
-  let
-    path = fromMaybe "." mPath
-  repoPath <- getRepoRoot path >>= \case
-    Left _ -> die "Not a git repo"
-    Right a -> pure a
-  isRepo <- isSecurityAdvisoriesRepo repoPath
-  unless isRepo $
-    die "Not a security-advisories repo"
-
-  getGreatestId repoPath >>= getNextHsecId >>= putStrLn . printHsecId
+runNextIDCommand mPath =
+  ensureRepo mPath >>= getGreatestId >>= getNextHsecId >>= putStrLn . printHsecId
